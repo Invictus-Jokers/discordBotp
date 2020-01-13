a2b3ca596977fe175aba07aa3613dffc7f36727b
zg3d <devzs6.98@gmail.com>
2020-01-13 12:26:14 -0500

    updated to discord.js
    discord.js added
    current fucntions
    !kick
    and ping

--- bot.js
+++ bot.js
@@ -1,39 +1,25 @@
-let Discord = require('discord.io'); 
-let logger = require('winston');
-let auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = 'bottoken here';
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-let bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        let args = message.substring(1).split(' ');
-        let cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+
+    } else if (msg.content.startsWith('!kick')) {
+        if (msg.mentions.users.size) {
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+        } else {
+            msg.reply('Please tag a valid user!');
+        }
+    }
 });
