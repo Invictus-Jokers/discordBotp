2d3f8e874878d2f68c1eacb1e19dafdca4875a40
zg3d <devzs6.98@gmail.com>
2020-01-15 23:12:00 -0500

    tried using aysnyc await
    curently a bug for heads and tails

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
+const mergeImg = require('merge-img');
 require('dotenv').config();
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 const bot = new Discord.Client();
 const TOKEN = 'token here';
 
@@ -9,41 +11,49 @@ bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-let flip = (msg)=>{
+let flip = async (msg) => {
     let msgArr = msg.content.split(" ");
-    if(msgArr.length <= 2){
-        let multiple =  Number(msgArr[1]);
-        if(msgArr.length === 1)
-        {
+    let send;
+    if (msgArr.length <= 2) {
+        let multiple = Number(msgArr[1]);
+        if (msgArr.length === 1) {
             multiple = 1;
         }
         let tail = "img/tail.png";
         let head = "img/head.png";
         let returnA = [];
-        if(multiple === NaN)
-        {
+        if (multiple === NaN) {
             multiple = 1;
         }
         console.log(multiple);
-        for(let i = 0 ; i< multiple; i++)
-        {
-            let random  = Math.round(Math.random());
-            if(random === 0) returnA.push(tail);
+        for (let i = 0; i < multiple; i++) {
+            let random = Math.round(Math.random());
+            if (random === 0) returnA.push(tail);
             else returnA.push(head);
+
+
         }
 
 
-        mergeImg(returnA).then((img) => {    // Save image as file
-        img.write('out.png', () => console.log('done'));
-    });
-        msg.channel.send( {files: ['out.png']});
-      //  console.log(returnA);
-        
-  
-    } else {
-        msg.channel.send("invalid message");
-    }
+        let out = await mergeImg(returnA, { Canvas: Canvas })
+        // Save image as file
+        console.log(out);
+        out.write('img/out.png', () => console.log('done'))
 
+
+        send = { files: ['img/out.png'] };
+
+
+
+
+
+        //  console.log(returnA);
+
+
+    } else {
+        send = "invalid message";
+    }
+    msg.channel.send(send);
 
 }
 
@@ -52,8 +62,8 @@ bot.on('message', msg => {
         msg.reply('pong');
         msg.channel.send('pong');
 
-    } 
-    
+    }
+
     if (msg.content.startsWith('!kick')) {
         if (msg.mentions.users.size) {
             const taggedUser = msg.mentions.users.first();
@@ -61,12 +71,12 @@ bot.on('message', msg => {
         } else {
             msg.reply('Please tag a valid user!');
         }
-    } 
-    
-    if (msg.content.startsWith('!flip')){        
+    }
+
+    if (msg.content.startsWith('!flip')) {
         flip(msg);
     }
-    
+
     if (msg.content.toLowerCase() == 'hello' || msg.content.toLowerCase() === 'hi') {
         msg.channel.send('Hello! Welcome to the Seneca Practice Bot');
     }
